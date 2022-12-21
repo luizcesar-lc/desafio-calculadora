414d3318ff07bafabce6b77c3093592b8d655490
luiz_ <luiizcesar.lc@gmail.com>
2022-12-21 16:02:15 -0300

    Desafio da DIO

---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #d98e04;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 24px;
+  font-family: 'Roboto';
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #d98e04;
+    border: 0;
+    display: flex;
+    text-align: right;
+    padding: 0 10px;
+    font-size: 24px;
+    font-family: 'Roboto';
+    color: #ffffff;
+  }
+`;
